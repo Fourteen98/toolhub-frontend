5cd57d31274832a43cd9caad3d7bca765edabe61
Muhyideen Elias <emuhiydeen6@gmail.com>
2022-10-17 12:16:02 +0000

    Create Home Page::implement navbar Add a logo, navbar title and navlinks to the navbar.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Navbar from './pages/components/Navbar';
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
