9c99dcc9fe1515a689daa16b721f6f8baeed2610
Muhyideen Elias <emuhiydeen6@gmail.com>
2022-10-18 07:48:13 +0000

    add link to logo

--- src/pages/Leaderboard.js
+++ src/pages/Leaderboard.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Leaderboard = () => (
 
-  <>
+  <div className="under--construction">
     <h1>Leaderboard Under Construction..</h1>
-  </>
+  </div>
 );
 
 export default Leaderboard;
